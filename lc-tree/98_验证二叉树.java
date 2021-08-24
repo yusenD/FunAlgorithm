c1699d8f022271763287b429d25479ae00d14c20
luyuan <tangchao36@163.com>
2021-08-25 00:58:32 +0800

    tree

---
+++ lc-tree/98_验证二叉树.java
@@ -0,0 +1,22 @@
+class Solution {
+    TreeNode max;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        // 左
+        boolean left = isValidBST(root.left);
+        if (!left) {
+            return false;
+        }
+        // 中
+        if (max != null && root.val <= max.val) {
+            return false;
+        }
+        max = root;
+        // 右
+        boolean right = isValidBST(root.right);
+        return right;
+    }
+
+}
