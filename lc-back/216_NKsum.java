d61926161386d4de458e8e16ed88b1ca1e31d4b6
luyuan <tangchao36@163.com>
2021-08-31 01:10:51 +0800

    rollback

--- lc-back/216_NKsum.java
+++ lc-back/216_NKsum.java
@@ -1,31 +1,33 @@
 class Solution {
-    List<List<Integer>> res = new ArrayList<>();
-    List<Integer> list = new ArrayList<>();
+	List<List<Integer>> result = new ArrayList<>();
+	LinkedList<Integer> path = new LinkedList<>();
 
-    public List<List<Integer>> combinationSum3(int k, int n) {
-        res.clear();
-        list.clear();
-        backtracking(k, n, 9);
-        return res;
-    }
+	public List<List<Integer>> combinationSum3(int k, int n) {
+		backTracking(n, k, 1, 0);
+		return result;
+	}
 
-    private void backtracking(int k, int n, int maxNum) {
-        if (k == 0 && n == 0) {
-            res.add(new ArrayList<>(list));
-            return;
-        }
+	private void backTracking(int targetSum, int k, int startIndex, int sum) {
+		// 减枝
+		if (sum > targetSum) {
+			return;
+		}
 
-        // 因为不能重复，并且单个数字最大值是maxNum，所以sum最大值为
-        // （maxNum + (maxNum - 1) + ... + (maxNum - k + 1)） == k * maxNum - k*(k - 1) / 2
-        if (maxNum == 0
-                || n > k * maxNum - k * (k - 1) / 2
-                || n < (1 + k) * k / 2) {
-            return;
-        }
-        list.add(maxNum);
-        backtracking(k - 1, n - maxNum, maxNum - 1);
-        list.remove(list.size() - 1);
-        backtracking(k, n, maxNum - 1);
-    }
-
+		if (path.size() == k) {
+            if (sum == targetSum) result.add(new ArrayList<>(path));
+			return;
+		}
+		
+		// 减枝 9 - (k - path.size()) + 1 这里需要考虑，如果还剩path里面还有两个位置，那么倒数第二个必然不能抢了倒数第一的数字
+		for (int i = startIndex; i <= 9 - (k - path.size()) + 1; i++) {
+			path.add(i);
+			sum += i;
+			backTracking(targetSum, k, i + 1, sum);
+            //回溯
+            path.removeLast();
+            
+			//回溯
+			sum -= i;
+		}
+	}
 }
